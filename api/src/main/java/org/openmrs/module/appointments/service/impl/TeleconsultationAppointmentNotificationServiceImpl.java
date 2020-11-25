9217b3e69dd36eedda9d06b674ab95c9586e9e86
Mahesh Ippili <maheshippili@Maheshs-MacBook-Pro.local>
2020-11-25 11:14:45 +0530

    make email send with logo

--- api/src/main/java/org/openmrs/module/appointments/service/impl/TeleconsultationAppointmentNotificationServiceImpl.java
+++ api/src/main/java/org/openmrs/module/appointments/service/impl/TeleconsultationAppointmentNotificationServiceImpl.java
@@ -5,23 +5,27 @@ import org.apache.commons.logging.LogFactory;
 import org.bahmni.module.email.notification.EmailNotificationException;
 import org.bahmni.module.email.notification.service.EmailNotificationService;
 import org.openmrs.Patient;
-import org.openmrs.PersonAttribute;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.appointments.model.AppointmentProvider;
 import org.openmrs.module.appointments.service.TeleconsultationAppointmentNotificationService;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Properties;
 
 public class TeleconsultationAppointmentNotificationServiceImpl implements TeleconsultationAppointmentNotificationService {
     private final static String EMAIL_SUBJECT = "teleconsultation.appointment.email.subject";
     private final static String EMAIL_BODY = "teleconsultation.appointment.email.body";
+    private final static String EMAIL_LOGO = "teleconsultation.appointment.email.logo";
 
     private Log log = LogFactory.getLog(this.getClass());
 
     private EmailNotificationService emailNotificationService;
+
+    private EmailTemplateConfig emailTemplateConfig = new EmailTemplateConfig();
 
     private TeleconsultationAppointmentService teleconsultationAppointmentService = new TeleconsultationAppointmentService();
 
@@ -34,43 +38,66 @@ public class TeleconsultationAppointmentNotificationServiceImpl implements Telec
     public void sendTeleconsultationAppointmentLinkEmail(Appointment appointment) throws EmailNotificationException {
         String link = teleconsultationAppointmentService.getTeleconsultationURL(appointment);
         Patient patient = appointment.getPatient();
-        PersonAttribute patientEmailAttribute = patient.getAttribute("email");
-        if (patientEmailAttribute != null) {
-            String email = patientEmailAttribute.getValue();
-            String patientName = appointment.getPatient().getGivenName();
-            String doctor = "";
-            if(appointment.getProviders() != null) {
-                AppointmentProvider provider = appointment.getProviders().iterator().next();
-                doctor = " with Dr. " + provider.getProvider().getPerson().getGivenName();
+        String email = patient.getAttribute("email").getValue();
+        try {
+            if (email != null) {
+                String patientName = appointment.getPatient().getGivenName();
+                String doctor = "";
+                if (appointment.getProviders() != null) {
+                    AppointmentProvider provider = appointment.getProviders().iterator().next();
+                    doctor = " with Dr. " + provider.getProvider().getPerson().getGivenName();
+                }
+                Date appointmentStart = appointment.getStartDateTime();
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(appointmentStart);
+                calendar.add(Calendar.HOUR_OF_DAY, 5);
+                calendar.add(Calendar.MINUTE, 30);
+                appointmentStart = calendar.getTime();
+                String day = new SimpleDateFormat("EEEE").format(appointmentStart);
+                String date = new SimpleDateFormat("dd/MM/yy").format(appointmentStart);
+                String time = new SimpleDateFormat("hh:mm a").format(appointmentStart);
+
+                Properties properties = emailTemplateConfig.getProperties();
+                String emailSubject = null;
+                String emailBody = null;
+                String emailLogo = null;
+                if (properties != null) {
+                    emailBody = properties.getProperty("email.body");
+                    emailSubject = properties.getProperty("email.subject");
+                    emailLogo = properties.getProperty("email.logo");
+                }
+                else {
+                    emailBody = EMAIL_BODY;
+                    emailSubject = EMAIL_SUBJECT;
+                    emailLogo = EMAIL_LOGO;
+                }
+
+
+                emailNotificationService.send(
+                        Context.getMessageSourceService().getMessage(emailSubject, null, null),
+                        Context.getMessageSourceService().getMessage(
+                                emailBody,
+                                new Object[]{
+                                        patientName,
+                                        doctor,
+                                        day,
+                                        date,
+                                        time,
+                                        link
+                                },
+                                null
+                        ),
+                        new String[]{email},
+                        null,
+                        null,
+                        emailLogo);
+                log.warn("Attempting to send an email to a patient with an email address 111");
+
+            } else {
+                log.warn("Attempting to send an email to a patient without an email address");
             }
-            Date appointmentStart = appointment.getStartDateTime();
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(appointmentStart);
-            calendar.add(Calendar.HOUR_OF_DAY, 5);
-            calendar.add(Calendar.MINUTE, 30);
-            appointmentStart = calendar.getTime();
-            String day = new SimpleDateFormat("EEEE").format(appointmentStart);
-            String date = new SimpleDateFormat("dd/MM/yy").format(appointmentStart);
-            String time = new SimpleDateFormat("hh:mm a").format(appointmentStart);
-            emailNotificationService.send(
-                    Context.getMessageSourceService().getMessage(EMAIL_SUBJECT, null, null),
-                    Context.getMessageSourceService().getMessage(
-                            EMAIL_BODY,
-                            new Object[]{
-                                    patientName,
-                                    doctor,
-                                    day,
-                                    date,
-                                    time,
-                                    link
-                            },
-                            null
-                    ),
-                    new String[] { email },
-                    null,
-                    null);
-        } else {
-            log.warn("Attempting to send an email to a patient without an email address");
+        }catch (IOException e){
+            throw new EmailNotificationException("Unable to load email-notification.properties, see details in README", e);
         }
     }
 
